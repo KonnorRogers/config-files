8b34baa707f8a70048b3c8b44ebb4bdc3831e07e
paramagicdev <konnor5456@gmail.com>
2019-11-25 16:55:32 -0500

    adding postgres scripts

--- scripts/postgres-md5-require.sh
+++ scripts/postgres-md5-require.sh
@@ -5,11 +5,14 @@ shopt -s nullglob
 
 array=(/etc/postgresql/*/main/pg_hba.conf)
 
-printf "Changing (peer | trust) auth options to md5 in the following directories\n\n"
+postgres_options="trust\|peer\|reject\|password\|gss\|sspi\|ident\|peer\|pam\|ldap\|radius\|cert"
+printf "Changing all auth options to md5 in the following directories\n\n"
 
 for dir in "${array[@]}"; do
   echo "$dir"
-  sudo sed -i 's/^\([^#]*\)peer\|trust/\1md5/g' "$dir"
+  sudo sed -i "s/^\([^#]*\)$postgres_options/\1md5/g" "$dir"
 done
 
 sudo /etc/init.d/postgresql restart
+
+printf "\n Your postgres now uses md5 based passwords!"
--- scripts/postgres-pass-reset.sh
+++ scripts/postgres-pass-reset.sh
@@ -5,14 +5,16 @@ shopt -s nullglob
 
 array=(/etc/postgresql/*/main/pg_hba.conf)
 
-printf "Changing (md5 | peer) auth options to 'trust' in the following directories...\n\n"
+printf "Changing postgres auth options to 'trust' in the following directories...\n\n"
+
+postgres_options="md5\|peer\|reject\|password\|gss\|sspi\|ident\|peer\|pam\|ldap\|radius\|cert"
 for dir in "${array[@]}"; do
   echo "$dir"
-  sudo sed -i 's/^\([^#]*\)md5\|peer/\1trust/g' "$dir"
+  sudo sed -i "s/^\([^#]*\)$postgres_options/\1trust/g" "$dir"
 done
 
 sudo /etc/init.d/postgresql restart
-printf "To change your password run the following:\n\n"
+printf "\n\nTo change your password run the following:\n\n"
 
 echo "psql -U postgres"
 echo "alter user postgres with password 'NEW_PASSWORD'"
