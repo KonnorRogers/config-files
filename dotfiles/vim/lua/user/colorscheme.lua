5e6e6e59b0f4805e687dd34a34c5cf50b99f2c74
Konnor Rogers <konnor5456@gmail.com>
2023-05-11 11:04:56 -0400

    auto push files

--- dotfiles/vim/lua/user/colorscheme.lua
+++ dotfiles/vim/lua/user/colorscheme.lua
@@ -1,5 +1,5 @@
--- local colorscheme = "xcodelighthc"
-local colorscheme = "xcodedarkhc"
+local colorscheme = "xcodelighthc"
+-- local colorscheme = "xcodedarkhc"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
