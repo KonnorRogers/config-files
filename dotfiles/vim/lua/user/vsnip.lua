18a5ed305a9231c60d35cc14cdece07111655001
Konnor Rogers <konnor5456@gmail.com>
2023-07-17 18:58:02 -0400

    auto push files

--- dotfiles/vim/lua/user/vsnip.lua
+++ dotfiles/vim/lua/user/vsnip.lua
@@ -1,28 +1,20 @@
+-- -- If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
+vim.g.vsnip_snippet_dir = vim.fn.expand('~/.vim/snippets')
+
+local keymap = vim.keymap
+
+keymap.set({"i", "s"}, "<expr> <C-k>", "vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<C-k>'")
+keymap.set({"i", "s"}, "<expr> <C-j>", "vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<C-j>'")
+keymap.set({"i", "s"}, "<expr> <C-y>",   "vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-y>'")
+
+-- Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+-- See https://github.com/hrsh7th/vim-vsnip/pull/50
+keymap.set({"n", "x"}, "s", "<Plug>(vsnip-select-text)")
+keymap.set({"n", "x"}, "S", "<Plug>(vsnip-cut-text)")
+
 vim.cmd([[
-" If you want to use snippet for multiple filetypes, you can `g:vsnip_filetypes` for it.
-let g:vsnip_snippet_dir = expand('~/.vim/snippets')
 let g:vsnip_filetypes = {}
 let g:vsnip_filetypes.typescript = ["javascript", "typescriptreact", "javascriptreact"]
 let g:vsnip_filetypes.javascriptreact = ["javascript"]
 let g:vsnip_filetypes.typescriptreact = ["typescript", "typescriptreact", "javascriptreact"]
-
-" NOTE: You can use other key to expand snippet.
-
-" jump
-
-imap <expr> <C-k>   vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<C-k>'
-smap <expr> <C-k>   vsnip#jumpable(1)  ? '<Plug>(vsnip-jump-next)' : '<C-k>'
-imap <expr> <C-j> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<C-j>'
-smap <expr> <C-j> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<C-j>'
-
-imap <expr> <C-y>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-y>'
-smap <expr> <C-y>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-y>'
-
-" Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
-" See https://github.com/hrsh7th/vim-vsnip/pull/50
-nmap        s   <Plug>(vsnip-select-text)
-xmap        s   <Plug>(vsnip-select-text)
-nmap        S   <Plug>(vsnip-cut-text)
-xmap        S   <Plug>(vsnip-cut-text)
 ]])
-
