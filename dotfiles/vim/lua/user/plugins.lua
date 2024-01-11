818118b81ec0b49d427d81e2a59d69924fbd010f
Konnor Rogers <konnor5456@gmail.com>
2024-01-11 02:26:26 -0500

    auto push files

--- dotfiles/vim/lua/user/plugins.lua
+++ dotfiles/vim/lua/user/plugins.lua
@@ -1,154 +1,147 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+-- Automatically install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+-- vim.opt.rtp:prepend(lazypath)
+-- vim.loader.enable()
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
+local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
   return
 end
 
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
+-- Install your plugins here
+return lazy.setup({
+  -- Colorscheme
+  {
+    "arzg/vim-colors-xcode",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      local colorscheme = os.getenv("COLORSCHEME") or "xcodelighthc"
+      vim.cmd.colorscheme(colorscheme)
     end,
   },
-  git = {
-    clone_timeout = 300, -- Timeout, in seconds, for git clones
+  -- Other color stuff
+  { "ap/vim-css-color" }, -- Add color to .css files
+  { "p00f/nvim-ts-rainbow" }, -- Rainbows!
+
+  -- Git related plugins
+  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-rhubarb' },
+
+  -- Tpope
+  { 'tpope/vim-surround' },
+  { 'tpope/vim-ragtag' },
+
+  -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' },
+
+  { "mattn/emmet-vim" }, -- HTML shortcuts
+  { "nvim-lua/plenary.nvim" }, -- Useful lua functions used by lots of plugins
+  { "nvim-lua/popup.nvim" }, -- Experimental popup api
+  { "numToStr/Comment.nvim" }, -- Comment stuff
+  { "JoosepAlviste/nvim-ts-context-commentstring" }, -- Treesitter extension
+
+  -- LSP Configuration & Plugins
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      { 'folke/neodev.nvim' },
+    },
   },
-}
-
--- Install your plugins here
-return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
-  use "numToStr/Comment.nvim" -- Comment stuff
-  use "JoosepAlviste/nvim-ts-context-commentstring" -- Treesitter extension
-  use "lewis6991/impatient.nvim" -- Faster loading
-  use "lukas-reineke/indent-blankline.nvim"
-  use "goolord/alpha-nvim"
-  use "nvim-lua/popup.nvim" -- Experimental popup api
-  use "folke/neodev.nvim" -- LSP stuff for plugins
-  use "b0o/schemastore.nvim" -- JSON schemas
-	use "folke/which-key.nvim" -- Show a menu of keys available
-
-  -- Colorschemes
-  use "lunarvim/lunar.nvim"
-  use "arzg/vim-colors-xcode"
-  use "mattn/emmet-vim"
-  use "ap/vim-css-color" -- Add color to .css files
-	-- use "p00f/nvim-ts-rainbow" -- Rainbows!
-
-	use {
+  { "lukas-reineke/indent-blankline.nvim" },
+  { "goolord/alpha-nvim" }, -- Startup screen
+  { "b0o/schemastore.nvim" }, -- JSON schemas
+  -- { 'folke/which-key.nvim', opts = {} }, -- Show a menu of keys available
+  {
   	"ray-x/lsp_signature.nvim",
   	event = "BufRead",
   	config = function() require"lsp_signature".on_attach() end,
-  }
+  },
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp", -- The completion plugin
+    dependencies = {
+      -- snippets
+      { 'hrsh7th/vim-vsnip' },
+      { 'hrsh7th/vim-vsnip-integ' },
+      { "rafamadriz/friendly-snippets" }, -- a bunch of snippets to use
 
-  -- cmp plugins
-  use {
-  	"hrsh7th/nvim-cmp", -- The completion plugin
-  	requires = {
-  		"hrsh7th/cmp-buffer", -- buffer completions
-  		"hrsh7th/cmp-path", -- path completions
-  		"hrsh7th/cmp-vsnip", -- snippet completions
-  		"hrsh7th/cmp-nvim-lsp",
-  		"hrsh7th/cmp-nvim-lua",
-  	}
-  }
-  -- snippets
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/vim-vsnip-integ'
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
-  -- LSP
-    use { -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    requires = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim', tag = "legacy" },
-    },
-  }
-
-  use { 'mihyaeru21/nvim-lspconfig-bundler', requires = 'neovim/nvim-lspconfig' }
-
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  use "Hoffs/omnisharp-extended-lsp.nvim"
-
-	-- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
+      -- Completions
+    	{ "hrsh7th/cmp-buffer" }, -- buffer completions
+    	{ "hrsh7th/cmp-path" }, -- path completions
+    	{ "hrsh7th/cmp-vsnip" }, -- snippet completions
+    	{ "hrsh7th/cmp-nvim-lsp" },
+    	{ "hrsh7th/cmp-nvim-lua" },
+    }
+  },
+  { "jose-elias-alvarez/null-ls.nvim" },-- for formatters and linters
+--   use "Hoffs/omnisharp-extended-lsp.nvim"
+--
+-- 	-- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    }
+  },
   -- Treesitter
-	use { -- Highlight, edit, and navigate code
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
-    requires = {
-      "Shopify/tree-sitter-liquid"
+    dependencies = {
+      { "Shopify/tree-sitter-liquid" },
+      { "nvim-treesitter/nvim-treesitter-textobjects" }
     }
-  }
-
-  use { -- Additional text objects via treesitter
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
-
+  },
   -- Git
-  use "lewis6991/gitsigns.nvim"
-  use({
-      "kdheepak/lazygit.nvim",
-      -- optional for floating window border decoration
-      requires = {
-          "nvim-lua/plenary.nvim",
-      },
-  })
-
-  -- DAP
-  -- use "mfussenegger/nvim-dap"
-  -- use "rcarriga/nvim-dap-ui"
-  -- use "ravenxrz/DAPInstall.nvim"
-
-  -- Tpope
-  use 'tpope/vim-surround'
-  use 'tpope/vim-ragtag'
-  use "tpope/vim-fugitive"
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+  { "lewis6991/gitsigns.nvim" },
+--   {
+--       "kdheepak/lazygit.nvim",
+--       -- optional for floating window border decoration
+--       dependencies = {
+--           "nvim-lua/plenary.nvim",
+--       },
+--   },
+--
+--   -- DAP
+--   -- use "mfussenegger/nvim-dap"
+--   -- use "rcarriga/nvim-dap-ui"
+--   -- use "ravenxrz/DAPInstall.nvim"
+--
+})
