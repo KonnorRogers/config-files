d9d903628c729453e07431b40639b7a53f80a3ab
Konnor Rogers <konnor5456@gmail.com>
2024-12-23 08:02:30 -0500

    auto push files

--- dotfiles/vim/lua/plugins/lsp_test.lua
+++ dotfiles/vim/lua/plugins/lsp_test.lua
@@ -1,20 +1,23 @@
--- local client = vim.lsp.start_client {
---   name = "test-lsp",
---   cmd = { vim.fn.expand("$HOME/projects/lsp-from-scratch/exports/server.js") },
---   -- on_attach = function (args)
---   -- end,
--- }
+local cmd = { 'node', vim.fn.expand("$HOME/projects/oss/webawesome-lsp/server/out/server.js"), '--', '--stdio' }
 
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "markdown",
---   callback = function ()
---     if not client then
---       vim.cmd.echo("\"Something went wrong\"")
---       return
---     end
+local client = vim.lsp.start_client {
+  name = "test-lsp",
+  cmd = cmd,
+  on_attach = function (args)
+  end,
+}
 
---     vim.lsp.buf_attach_client(0, client)
---   end
--- })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function ()
+    -- vim.cmd.echo(cmd)
+    if not client then
+      vim.cmd.echo("\"Something went wrong\"")
+      return
+    end
+
+    vim.lsp.buf_attach_client(0, client)
+  end
+})
 
 
