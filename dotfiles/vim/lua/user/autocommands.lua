afbeca8024c6dcaed993fe474bad1043b686e6af
Konnor Rogers <konnor5456@gmail.com>
2026-07-13 22:54:24 -0400

    auto push files

--- dotfiles/vim/lua/user/autocommands.lua
+++ dotfiles/vim/lua/user/autocommands.lua
@@ -44,7 +44,7 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
-		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+		vim.highlight.hl_op({ higroup = "Visual", timeout = 200 })
 	end,
 })
 
